ace7f4fa0c0ec862ed0c8beb9b21f1b1546afb76
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-16 18:59:15 +0200

    Crating a component in vanilla  Js

--- 01_Lessom/index.js
+++ 01_Lessom/index.js
@@ -1,15 +1,4 @@
-function Car(){
-    return <h2>I'm Learning Recat Js!</h2>
-}
-
-const root1 =  ReactDOM.createRoot(window.document.getElementById('root1'))
-root1.render(<Car/>)
-// ! There is another way 
-
-ReactDOM.render(
-    <div>
-        <Car/>
-    </div>,
-
-   window.document.getElementById('root')
-)
+const msgEl =  window.document.getElementById("root")
+const h1 =  window.document.createElement("h1")
+h1.textContent = "This is an imperative way to program!"
+msgEl.append(h1)
