1655e3bfddab38a1cf1fbbe6160fcc542c28cb49
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-16 19:35:19 +0200

    Different way to show a massage in Rjs
    I've created  a variable and give it a Html

--- 01_Lessom/index.js
+++ 01_Lessom/index.js
@@ -1,2 +1,4 @@
+const headerEl = <h1 className="header">This is The power of ReactJs!</h1>
+ReactDOM.render(headerEl, window.document.getElementById("root"))
 
-ReactDOM.render(<h1 className="header">This is The power of ReactJs!</h1>, window.document.getElementById("root"))
+console.log(headerEl)
