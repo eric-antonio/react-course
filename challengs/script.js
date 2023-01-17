82988cd446bfe2ff839a774807fccce5c4f64cbc
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-17 16:55:14 +0200

    Update script.js

---
+++ challengs/script.js
@@ -0,0 +1,13 @@
+ function PageComponent(){
+    return (
+    <div>
+        <ul>
+            <li>I want to become good in React</li>
+            <li>React it's awesome </li>
+        </ul>
+    </div>
+    )
+ }
+
+
+ReactDOM.render(<PageComponent/>, window.document.getElementById("root"))
