9ec185192555206919a62263a3077f0433a69750
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-17 13:09:48 +0200

    Solved the challenge

--- 01_Lessom/index.js
+++ 01_Lessom/index.js
@@ -1,14 +1,21 @@
-const page = (
+const pageEl = (
+    
     <div>
-        <h1>My awesome website in React </h1>
-        <h3>Reasons Wy i love React </h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's declarative</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
+        <img id="image" src="react-logo.png" width="40px"></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>was originally created bj jordan walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>In maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile
+                apps.
+            </li>
 
+        </ul>
     </div>
+
 )
-ReactDOM.render(page,window.document.getElementById("root"))
+
+
+ReactDOM.render(pageEl,window.document.getElementById("root"))
