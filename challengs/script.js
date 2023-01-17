0d831fa5d3be32ddce69928e44897c7aeb7fd26b
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-17 17:28:28 +0200

    Separate the components!
    Now i'm introducing another diferent way to apply react in my page !

--- challengs/script.js
+++ challengs/script.js
@@ -1,33 +1,54 @@
+// * Header Content 
 function HeaderEl(){
     return (
-        <na>
-            <img src="react-logo.png" alt="React JS"  width="40px"/>
-        </na>
+        <header>
+            
+            <na>
+                <img src="react-logo.png" alt="React JS"  width="40px"/>
+                <h1>Reasons I'm excited to learn React</h1>
+            </na>
+        </header>
+       
     )
- }
+}
 
-function PageComponent(){
-    return (
-    <div>
+// *Main Content 
+function MainContent(){
+    return(
+        <div>
         <ol>
             <li>I want to become good in React</li>
             <li>React it's awesome </li>
         </ol>
     </div>
     )
- }
+}
 
+
+// * Footer Element 
 function FooterEL(){
     return(
         <footer>
-            <a href="http://github.com/eric-antonio">@eric-antonio</a> 
-            development. All rights reserved.
+            <small>
+                <a href="http://github.com/eric-antonio">@2023 eric-antonio</a>
+                development. All rights reserved.
+            </small>
+            
         </footer>
     )
- }
+}
+
+function PageComponent(){
+    return (
+        <di>
+            <HeaderEl/>
+            <MainContent/>
+            <FooterEL/>
+        </di>
+        
+    )
+}
 
 
 // * This the way i can Apply components in to my page!
-ReactDOM.render(<HeaderEl/>, window.document.getElementById("header"))
-ReactDOM.render(<PageComponent/>, window.document.getElementById("main"))
-ReactDOM.render(<FooterEL/>,  window.document.getElementById("footer"))
+ReactDOM.render(<PageComponent/>, window.document.getElementById("root"))
