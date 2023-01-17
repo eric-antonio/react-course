6626cdebe4ebc8c2c9a2c4f45bc29e61d17c710c
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-17 17:08:39 +0200

    Creating more than on component and using

--- challengs/script.js
+++ challengs/script.js
@@ -1,13 +1,33 @@
+ function HeaderEl(){
+    return (
+        <na>
+            <img src="react-logo.png" alt="React JS"  width="40px"/>
+        </na>
+    )
+ }
+
  function PageComponent(){
     return (
     <div>
-        <ul>
+        <ol>
             <li>I want to become good in React</li>
             <li>React it's awesome </li>
-        </ul>
+        </ol>
     </div>
     )
  }
 
+ function FooterEL(){
+    return(
+        <footer>
+            <a href="http://github.com/eric-antonio">@eric-antonio</a> 
+            development. All rights reserved.
+        </footer>
+    )
+ }
 
-ReactDOM.render(<PageComponent/>, window.document.getElementById("root"))
+
+// * This the way i can Apply components in to my page!
+ReactDOM.render(<HeaderEl/>, window.document.getElementById("header"))
+ReactDOM.render(<PageComponent/>, window.document.getElementById("main"))
+ReactDOM.render(<FooterEL/>,  window.document.getElementById("footer"))
